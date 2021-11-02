56d1d61a6f1aad88b5510aef5b8ffcf1926f7df5
Imre-Vaczi <imre.vaczi@gmail.com>
2021-11-02 21:33:46 +0100

    MovieMain class updated

--- src/test/java/day01/MovieMain.java
+++ src/test/java/day01/MovieMain.java
@@ -1,7 +1,12 @@
 package day01;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MovieMain {
-    public static void main(String[] args) {
+    @Test
+    void main(){
         Movie movie = new Movie("Pi", 1998);
         movie.addActor(new Actor("Sean", 1974));
         movie.addActor(new Actor("Mark", 1970));
@@ -9,6 +14,6 @@ public class MovieMain {
         movie.addActor(new Actor("Samia", 1955));
         movie.addActor(new Actor("Ajay", 1990));
 
-        System.out.println(movie.actorsInTheirTwenties());
+        assertEquals(2, movie.actorsInTheirTwenties());
     }
 }
